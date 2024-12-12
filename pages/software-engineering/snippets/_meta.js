6b398c416fe6145d7b2f78b0567a680a2b29e46f
Irfan Maulana <mazipanneh@gmail.com>
2024-12-12 16:27:23 +0700

    FEAT: Upgrade Nextra 3.2 (#17)

--- pages/software-engineering/snippets/_meta.js
+++ pages/software-engineering/snippets/_meta.js
@@ -2,5 +2,6 @@ export default {
   "001-web-storage-helper": "❇️ Web Storage Helper",
   "002-device-id-with-fingerprint": "❇️ Device Id with FingerprintJS",
   "003-various-array-helpers": "❇️ Various Array Helpers",
-  "004-submit-event": "❇️ Getting Know the Submit Event"
+  "004-submit-event": "❇️ Getting Know the Submit Event",
+  "005-various-dom-access": "❇️ Various DOM Access"
 }
